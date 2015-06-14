04a4a0e97655a2cc103a267acc30ef5bbfd4578d
Antony <anthonykalogeropoulos@gmail.com>
2015-06-14 18:56:07 +0300

    initial commit

---
+++ app.java
@@ -0,0 +1,11 @@
+/**
+ * The App class implements an application that
+ * simply prints "Hello $name!" to standard output,
+ 	where $name is the name of the user.
+ */
+class App {
+    public static void main(String[] args) {
+    	
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
